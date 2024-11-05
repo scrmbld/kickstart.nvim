45bcd8b3b654ca61daf3487388ed2232e0ea8065
scrmbld <liamallen792@gmail.com>
2024-11-05 11:01:53 -0800

    move some plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,38 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here
+      vim.g.vimtex_view_method = 'zathura'
+    end,
+  },
+  -- jupyter notebook support
+  {
+    'kiyoon/jupynium.nvim',
+    build = 'pip3 install --user .',
+    -- build = "conda run --no-capture-output -n jupynium pip install .",
+    -- enabled = vim.fn.isdirectory(vim.fn.expand "~/miniconda3/envs/jupynium"),
+  },
+  -- MarkDown stuff
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    latex = {
+      enabled = true,
+      converter = 'latex2text',
+      highlight = 'RenderMarkdownMath',
+      top_pad = 0,
+      bottom_pad = 0,
+    },
+    opts = {},
+  },
+}
