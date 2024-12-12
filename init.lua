66e00f68aa91d59fa32a61cd94a32c16a9ca5b19
Cynthia Allen <cynthiaallen228@gmail.com>
2024-12-12 15:53:32 -0800

    add clangd

--- init.lua
+++ init.lua
@@ -636,6 +636,7 @@ require('lazy').setup({
         -- clangd = {},
         gopls = {},
         pyright = {},
+        clangd = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
